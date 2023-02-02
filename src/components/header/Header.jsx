fd3c25eaf1980fa7b229fa67d8f26ea5408499aa
Taryn King <marshaeking@gmail.com>
2023-02-01 22:37:04 -0600

    feat: add mobile side menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import NavBar from '../nav/NavBar';
 import logo from '../../assets/CrownLogo--dkbg.svg';
 import styles from './Header.module.css';
 
 const Header = () => {
+    const [windowDimension, setWindowDimension] = useState(null);
+
+    useEffect(() => {
+        setWindowDimension(window.innerWidth);
+    }, []);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowDimension(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const isMobile = windowDimension <= 640;
+
     return ( 
         <header className={styles.header}>
             <img className={styles.headerLogo}src={logo} alt='gradient crown logo' />
-            <NavBar />
+            <NavBar isMobile={isMobile}/>
         </header>
      );
 }
--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,14 +1,40 @@
+import { useState } from 'react';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
+import { MdClose } from 'react-icons/md';
 import styles from './NavBar.module.css';
 import resume from '../../assets/Taryn-King_Resume.pdf';
 
-const NavBar = () => {
+const NavBar = ({ isMobile }) => {
+     const [menuOpen, setMenuOpen] = useState(false);
+
+     const menuClick = () => {
+          setMenuOpen(!menuOpen);
+     }
+
     return ( 
-        <nav className={styles.navContainer}>
-             <li><a href='#about' className={styles.navLinks}>About</a></li>
-             <li><a href='#projects' className={styles.navLinks}>Work</a></li>
-             <li><a href='#contact' className={styles.navLinks}>Contact</a></li>
-             <li><a href={resume} target='_blank' rel="noreferrer" className={styles.resumeLink}>View Résumé</a></li>
-        </nav>
+     <>
+          {isMobile ? (
+               <div>
+                    {!menuOpen && <button className={styles.mobileMenuBtn} onClick={menuClick}><HiOutlineMenuAlt3 size={42}/></button> }
+                    { menuOpen && <aside tabindex='1' className={styles.mobileNavContainer}>
+                         <button className={styles.closeBtn} onClick={menuClick}><MdClose size={42}/></button>
+                         <nav className={styles.mobileNav}>
+                              <li><a href='/#about' className={styles.navLinks} onClick={menuClick}>About</a></li>
+                              <li><a onClick={menuClick}href='#projects' className={styles.navLinks}>Work</a></li>
+                              <li><a onClick={menuClick} href='#contact' className={styles.navLinks}>Contact</a></li>
+                              <li><a onClick={menuClick} href={resume} target='_blank' rel="noreferrer" className={styles.resumeLink}>View Résumé</a></li>
+                         </nav>
+                    </aside> }
+               </div>
+          ) : (
+               <nav className={styles.navContainer}>
+                    <li><a href='#about' className={styles.navLinks}>About</a></li>
+                    <li><a href='#projects' className={styles.navLinks}>Work</a></li>
+                    <li><a href='#contact' className={styles.navLinks}>Contact</a></li>
+                    <li><a href={resume} target='_blank' rel="noreferrer" className={styles.resumeLink}>View Résumé</a></li>
+               </nav>
+          )}
+          </>
      );
 }
  
