64306f75a7c3bdf74e52e56f2337beb68d23ff54
Taryn King <marshaeking@gmail.com>
2023-02-06 13:40:39 -0600

    refactor: make projectCard details dynamic instead of hard coded

--- src/components/work/ProjectCard.jsx
+++ src/components/work/ProjectCard.jsx
@@ -3,25 +3,29 @@ import { FiExternalLink } from 'react-icons/fi';
 
 import styles from './ProjectCard.module.css';
 
-const ProjectCard = () => {
+const ProjectCard = ({ data }) => {
+
     return ( 
         <li className={styles.projectCard}>
             <div className={`${styles.projectDetails}`}>
                 <p className={`${styles.projectTag} emphasis`}>Featured Project</p>
-                <h3 className={styles.projectTitle}>Portfolio Site</h3>
-                <div className={styles.description}>Dark themed portfolio site with a minimalist design. The purpose of this project is to provide a space where more can be learned about me and what I'm working on!</div>
+                <h3 className={styles.projectTitle}>{data.title}</h3>
+                <div className={styles.description}>{data.description}</div>
                 <ul className={`${styles.techStack} ${styles.flex}`}>
-                    <li>React</li>
-                    <li>Express</li>
-                    <li>EmailJS</li>
-                    <li>CSS Modules</li>
+                    { data.techStack.map( (tech) => {
+                        return <li>{tech}</li>
+                        }                      
+                    )}
+                    
                 </ul>
                 <ul className={`${styles.projectLinks} ${styles.flex}`}>
-                    <a href='https://github.com/TK1ng/kingdev-portfolio-lite' rel='noopener noreferrer' aria-label='External Link' target='_blank' ><FaGithub size={25}/></a>
-                    <a href='/' rel='noopener noreferrer' aria-label='External Link' target='_blank'><FiExternalLink size={25}/></a>
+                    <a href={data.ghUrl} rel='noopener noreferrer' aria-label='External Link' target='_blank' ><FaGithub size={25}/></a>
+                    { data.demoUrl && <a href={data.demoUrl} rel='noopener noreferrer' aria-label='External Link' target='_blank'><FiExternalLink size={25}/></a>}
                 </ul>
             </div>
-            <div className={styles.projectImg}><img src='https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80' alt='screenshot'/></div>
+            <div className={styles.projectImg}>
+                <img src={data.imgUrl} alt={data.imgAltTxt}/>
+            </div>
         </li>
      );
 }
