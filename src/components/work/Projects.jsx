b2738c3dc07536b2397b1b9542b9a5b3b38fb431
Taryn King <marshaeking@gmail.com>
2023-02-06 14:53:40 -0600

    add demoUrl for redesign project

--- src/components/work/Projects.jsx
+++ src/components/work/Projects.jsx
@@ -10,7 +10,7 @@ const Work = () => {
             projectId: "2", title: "Movie Watchlist App", description: `An application that consumes The Movie Database API to present popular movie titles that can be added to a watch list.`, imgAltTxt: "Screenshot of movie app displaying latest movie posters", imgUrl: "https://github.com/TK1ng/movie-watchlist/blob/main/public/movie-watchlist.png?raw=true", ghUrl: "https://github.com/TK1ng/movie-watchlist", demoUrl: "https://king-movie-watchlist.netlify.app", isFeatured: true, techStack: ['ReactJs', 'Axios', 'CSS Modules'], date: "01-23"
         },
         {
-            projectId: "3", title: "Website Redesign: Soirée Coffee Bar", description: "Restaurant website redesign for a local Dallas coffee bar.", imgAltTxt: "Restaurant website landing page", imgUrl: "https://github.com/TK1ng/kingdev-portfolio-lite/blob/main/src/assets/soiree_footer.png?raw=true", ghUrl: "", demoUrl: "", isFeatured: true, techStack: ['Figma','JavaScript', 'Pure CSS'], date: "06-21"
+            projectId: "3", title: "Website Redesign: Soirée Coffee Bar", description: "Restaurant website redesign for a local Dallas coffee bar.", imgAltTxt: "Restaurant website landing page", imgUrl: "https://github.com/TK1ng/kingdev-portfolio-lite/blob/main/src/assets/soiree_footer.png?raw=true", ghUrl: "", demoUrl: "https://www.figma.com/file/bWdk97QHRIBTW7mHtK0nmE/Draft?node-id=1%3A3", isFeatured: true, techStack: ['Figma','JavaScript', 'Pure CSS'], date: "06-21"
         },
         // {
         //     projectId: "4", title: "", description: "", imgAltTxt: "", imgUrl: "", ghUrl: "", demoUrl: "", isFeatured: true
