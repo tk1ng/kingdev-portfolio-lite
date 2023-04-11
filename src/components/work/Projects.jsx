d541cf82d1076457440f91e707b0a1e062480069
Taryn King <marshaeking@gmail.com>
2023-04-10 21:32:23 -0500

    add new project details

--- src/components/work/Projects.jsx
+++ src/components/work/Projects.jsx
@@ -12,9 +12,9 @@ const Work = () => {
         {
             projectId: "3", title: "Website Redesign: Soirée Coffee Bar", description: "Restaurant website redesign for a local Dallas coffee bar.", imgAltTxt: "Restaurant website landing page", imgUrl: "https://github.com/TK1ng/kingdev-portfolio-lite/blob/main/src/assets/soiree_footer.png?raw=true", ghUrl: "", demoUrl: "https://www.figma.com/file/bWdk97QHRIBTW7mHtK0nmE/Draft?node-id=1%3A3", isFeatured: true, techStack: ['Figma','JavaScript', 'Pure CSS'], date: "06-21"
         },
-        // {
-        //     projectId: "4", title: "", description: "", imgAltTxt: "", imgUrl: "", ghUrl: "", demoUrl: "", isFeatured: true
-        // }
+        {
+            projectId: "4", title: "B E L L Y APP", description: "Full stack health and wellness app that tracks meals and wellness scores to help understand the correlation between diet and health.", imgAltTxt: "", imgUrl: "https://github.com/TK1ng/kingdev-portfolio-lite/blob/main/src/assets/bellyapp.png?raw=true", ghUrl: "", demoUrl: "", isFeatured: true, techStack: ['ReactJS','NodeJS', 'ExpressJS',  'PostgreSQL'], date: "02-23"
+        }
     ];
 
     const displayProjectCards = projects.map(project => {
