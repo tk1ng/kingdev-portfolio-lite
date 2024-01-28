72548c28054e39d96f3c8bcd144d06838073e8b4
Taryn King <marshaeking@gmail.com>
2024-01-28 14:22:23 -0600

    chore: update headline name

--- src/components/headline/Headline.jsx
+++ src/components/headline/Headline.jsx
@@ -3,7 +3,7 @@ import styles from './Headline.module.css';
 const Headline = () => {
     return ( 
         <div className={styles.headline}>
-          <h1>I am <span className={styles.gradient}>King</span><span className={styles.title}>Developer</span></h1>
+          <h1>Taryn <span className={styles.gradient}>King</span><span className={styles.title}>Developer</span></h1>
         </div>
      );
 }
