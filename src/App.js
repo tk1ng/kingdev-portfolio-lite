4f73de21b09dd5467a7b9185abca1a852c12b81e
Taryn King <marshaeking@gmail.com>
2024-03-28 09:30:05 -0500

    refactor landing page style. closes #2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ContactForm from './components/contact/ContactForm';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import HeadLine from './components/headline/Headline';
+import HeroWrapper from './components/ui/Hero/HeroWrapper';
 import Projects from './components/work/Projects';
 import SocialLinks from './components/socialLinks/SocialLinks';
 import styles from './App.module.css';
@@ -11,12 +12,14 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.App}>
-      <section className={styles.landing}>
-        <Header />
+      <Header />
+      <HeroWrapper>
         <HeadLine />
-        <Button cta="Projects" primary onClickAction={() => window.location.href = '#projects'} />
-        <Button cta="Contact Me" onClickAction={() => window.location.href = '#contact'} />
-      </section>
+        <div className={styles.btnContainer}>
+          <Button cta="Projects" primary onClickAction={() => window.location.href = '#projects'} />
+          <Button cta="Contact" onClickAction={() => window.location.href = '#contact'} />
+        </div>
+      </HeroWrapper>
       <section>
         <About />
       </section>
